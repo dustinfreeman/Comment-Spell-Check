c8b91ac20b1e56b4d3689df68af076ec32519851
Joe Pasqualetti <public_github@joepasq.com>
2014-12-22 19:25:08 -0500

    Pickup project with unsaved changes(!). Set comments in slightly and title-case:
    - Change detection of if a word is in ignored list.
    - Can't believe I left this with unsaved changes!
    - Rediscovered via gist.github.com/joepasq while working on NY DoT Projects app.

--- csc.rb
+++ csc.rb
@@ -1,49 +1,54 @@
 #!/usr/bin/env ruby
 require 'raspell' 
 
+# List of words we can safely ignore as language constructs, language keywords, jargon, or documentation keywords 
+$ignored_words = ['struct', 'iOS', 'rect', 'param']
+
 class CommentSpellCheck
   SP = Aspell.new("en") #initialize our spell checker
   SP.suggestion_mode = Aspell::NORMAL
   SP.set_option("ignore-case", "false") #case sensitivity is important to us
 
-  #regex to find text within documentation code comments, not plain // comments or regular block comments /* */. Only /** */ comments
-  @@comment_regex    = /\/\*\*([\w\d\W\D]+?)\*\//
-  #list of words we can safely ignore as language constructs, language keywords, jargon, or documentation keywords 
-  @@ignored_words    = ['struct', 'iOS', 'rect', 'param']
-
-  #find all the misspellings and potential errors in a file
+  # Regex to find text within documentation code comments, not plain // comments or regular block comments /* */. Only /** */ comments
+  @@comment_regex    = /\/\*([\w\d\W\D]+?)\*\//
+ 
+  # Find all the misspellings and potential errors in a file
   def self.misspelledWords(string, path)
-  	#store all the wrong words and potential errors
+  	# Store all the wrong words and potential errors
   	wrongWords = Array.new
   	probablySpeltRight = Array.new
-  	#determine what file we are in by getting the last element of the path when split by forward slashes
+  	# Determine what file we are in by getting the last element of the path when split by forward slashes
   	filename = path.split('/')[-1]
 
-  	#split the file by the comment finding regex
+  	# Split the file by the comment finding regex
   	string.scan(@@comment_regex) {|comment|
   		for line in comment #find each line of a comment
   			for word in line.split(' ') #find each word of a line of a comment
   				word.gsub!(/([\W\d]+)/, '') #cut out non-word and numeric characters
-  				#I sure hope you don't use numbers in your variable names!!!
+  				# I sure hope you don't use numbers in your variable names
   				word.strip! #strip out any whitespace, just in case
 
-  				#iterate through the list of ignoreable words and ignore any ones we meet
-  				@@ignored_words.each { |ignoreMe| 
-  					if word == ignoreMe
-  						word = ''
-  					end
-  				}
+          if $ignored_words.include? word
+            word = ''
+            break
+          end
 
+          # Iterate through the list of ignoreable words and ignore any ones we meet
+  				# $ignored_words.each { |ignoreMe| 
+  				# 	if word == ignoreMe
+  				# 		word = ''
+      #         break
+      #       end
+  				# }
         
         first_suggestion = SP.suggest(word)[0]
         second_suggestion = SP.suggest(word)[1]
 
-
-  				#a camel case word is probably okay to ignore
-  				# being a variable or other programming construct
+  				# A camel case word is probably okay to ignore
+  				#  being a variable or other programming construct
 				if hasInnerCapital(word)
-					#add to list of probably safely ignored
-					#add the filename to the warned word indenting it first
+					# Add to list of probably safely ignored
+					# Add the filename to the warned word indenting it first
 					item = "\e[33m" + word + "\e[0m" + "\t in #{filename}"
           if first_suggestion != nil
             item += "\n did you mean \e[32m#{first_suggestion}\e[0m or \e[32m#{second_suggestion}\e[0m?"
@@ -54,37 +59,38 @@ class CommentSpellCheck
 					next
 				end
 
+				# Check if this word is: spelled wrong, ascii characters only, 
+				#  not a word with >2 capitals in a row signifying an Objective-C
+				#  namespace prefixed construct, and not equal to the empty string
+	  		
+          if isValidWord(word) # Switch this to `word.isValidWord?`
+  		    	# Add the filename to the misspelled word indenting it first
+            item = "\e[31m" + word + "\e[0m" + "\t in #{filename}" 
+            if first_suggestion != nil
+              item += "\n did you mean \e[35m#{first_suggestion}\e[0m or \e[35m#{second_suggestion}\e[0m?"
+            end
 
-				#check if this word is: spelled wrong, ascii characters only, 
-				# not a word with >2 capitals in a row signifying an Objective-C
-				# namespace prefixed construct, and not equal to the empty string
-	  		    if isValidWord(word) #switch this to `word.isValidWord?`
-	  		    	#add the filename to the misspelled word indenting it first
-              item = "\e[31m" + word + "\e[0m" + "\t in #{filename}" 
-              if first_suggestion != nil
-                item += "\n did you mean \e[35m#{first_suggestion}\e[0m or \e[35m#{second_suggestion}\e[0m?"
-              end
+  	    		wrongWords << item
+    	    end
 
-  		    		wrongWords << item
-		  	    end
 		  	end
   		end
   	}
 
-  	#make sure that each element only occurs once and return the errors and warnings
+  	# Make sure that each element only occurs once and return the errors and warnings
 	return wrongWords.uniq, probablySpeltRight.uniq 
   end
 
-  #check if this word is: spelled wrong, ascii characters only, 
-  # not a word with >2 capitals in a row signifying an Objective-C
-  # namespace prefixed construct, and not equal to the empty string
+  # Check if this word is: spelled wrong, ascii characters only, 
+  #  not a word with >2 capitals in a row signifying an Objective-C
+  #  namespace prefixed construct, and not equal to the empty string
   def self.isValidWord string
-  	return (!SP.check(string) and string.ascii_only? and !hasObjective_CNamePrefix(string)  and string != '')
+  	return (!SP.check(string) && string.ascii_only? && !hasObjective_CNamePrefix(string)  && string != '')
   end
 
-  #check if this string begins with two or more capitals
-  # which signifies that it is a string with an Objective-C
-  # namespace prefix, we ignore these
+  # Check if this string begins with two or more capitals
+  #  which signifies that it is a string with an Objective-C
+  #  namespace prefix, we ignore these
   def self.hasObjective_CNamePrefix string
   	capsCount = 0
   	for letter in string.each_char
@@ -100,7 +106,7 @@ class CommentSpellCheck
   	return false
   end
 
-  #check if there is an inner capitalization: exampleString
+  # Check if there is an inner capitalization: exampleString
   #  which are often used in variables
   #  used to put matches in potential errors list
   def self.hasInnerCapital string
@@ -113,36 +119,49 @@ class CommentSpellCheck
   end
 
   # Given a directory, it will scan it for all .h files and spell check comments 
-  def self.spellCheck(path = nil)
-    if path
-      path = File.expand_path(path)
-      allObjCFiles = Dir.glob(path + '/**/*.h') #recursively search path and subdirectories
+  def self.spellCheck(path = nil, ignoreFile = nil)
+    # Load a text file of other words to ignore and append it to class list of ignored words
+    if ignoreFile      
+      importedIgnoreFile = File.open(ignoreFile, 'r')
+      userIgnore = importedIgnoreFile.read.split('\n')
+      $ignored_words << userIgnore
+      $ignored_words.flatten!
+      importedIgnoreFile.close
     else
-      allObjCFiles = Dir.glob('**/*.h') #recursively search present directory
+      puts "No input of additional words to ignore."
     end
 
-    #store the results of each file's spell check split into errors and warnings
+    puts $ignored_words
+
+    if path
+      path = File.expand_path(path)
+      allObjCFiles = Dir.glob(path + '/**/*.h') # Recursively search path and subdirectories
+    else
+      allObjCFiles = Dir.glob('**/*.h') # Recursively search present directory
+    end
+
+    # Store the results of each file's spell check split into errors and warnings
     errors = Array.new
     potentialErrors = Array.new
 
-    #iterate through each file
+    # Iterate through each file
     allObjCFiles.each { |path| 
-      #read the file
+      # Read the file
       input = File.open(path, 'r')
       text = input.read
 
-      #calculate misspellings and potential misspellings
+      # Calculate misspellings and potential misspellings
       #  don't worry about duplicates or empty strings 
       #  as `misspelledWords() takes care of them
       misspellings, probablyRight = self.misspelledWords(text, path)
-		errors << misspellings
-		potentialErrors << probablyRight
+      errors << misspellings
+      potentialErrors << probablyRight
      
       input.close
     }
 
     if errors.count > 0
-	    #color Errors red, print each error
+	    # Color Errors red, print each error
 	    puts "\e[1mSpelling Errors:\e[0m"
 	    errors.each {|error|
 	    	puts error
@@ -152,7 +171,7 @@ class CommentSpellCheck
 	  end
 
   	if potentialErrors.count > 0
-  	    #color Warnings yellow, print each warning
+  	    # Color Warnings yellow, print each warning
   	    puts "\e[1m\nSpelling Warnings:\e[0m"
   	    potentialErrors.each {|perror|
   		    puts perror
@@ -163,9 +182,12 @@ class CommentSpellCheck
   end
 end
 
+# Go!
 path = ARGV[0]
+ignoredWordFile = ARGV[1]
 if path
-	CommentSpellCheck.spellCheck(path)
+	CommentSpellCheck.spellCheck(path, ignoredWordFile)
 else
-	CommentSpellCheck.spellCheck('.')
+	CommentSpellCheck.spellCheck('.', nil)
 end
+# 
